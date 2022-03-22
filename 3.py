140a9a9851e16d19825ee20f3474e3ea45ead6fb
patrycja2002 <80715862+patrycja2002@users.noreply.github.com>
2022-03-22 22:45:32 +0100

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,31 @@
+# 4
+
+przeg_1 = []
+przeg_2 = []
+przeg_3 = []
+
+szuf_1 = [przeg_1, przeg_2, przeg_3]
+szuf_2 = [przeg_1, przeg_2, przeg_3]
+szuf_3 = [przeg_1, przeg_2, przeg_3]
+szafka = [szuf_1, szuf_2, szuf_3]
+print(szafka)
+
+a = 'dlugopis'
+
+szafka[1][1] = a
+print(szafka)
+
+szafka_ = []
+szuflada_ = []
+przegrodka_ = []
+for y in range(3):
+    szuflada_.append(przegrodka_)
+for x in range(3):
+    szafka_.append(szuflada_)
+
+
+
+b = 'dlugopis'
+
+szafka_[1][1] = b
+print(szafka_)
