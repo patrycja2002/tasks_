d1b40d1f37c5a7b923ab3db51d626a6bfe6aeab7
patrycja2002 <80715862+patrycja2002@users.noreply.github.com>
2022-03-22 22:45:57 +0100

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,12 @@
+# 6
+book = {
+    123456789: "Jan Kot",
+    999888777: "Anna Lis",
+    111222333:  "Jan Kot"
+}
+print(book[123456789])
+
+# 8
+D = {1: 'Ala', 2: 'ma', 3: 'kota'}
+for key in D:
+    print(D[key])
